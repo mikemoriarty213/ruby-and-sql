c3f0f2abf34049ec557d1c8fc06891aef1b4b2c1
mikemoriarty213 <mike.moriarty@kellogg.northwestern.edu>
2023-01-31 21:57:37 +0000

    models lab complete!

--- labs/2-models.rb
+++ labs/2-models.rb
@@ -21,12 +21,37 @@ Salesperson.destroy_all
 
 # 2. insert 1-2 rows in salespeople table.
 
+new_salesperson = Salesperson.new
+
+new_salesperson["first_name"] = "Michael"
+new_salesperson["last_name"] = "Moriarty"
+new_salesperson["email"] = "michael.j.b.moriarty@gmail.com"
+new_salesperson.save
+
+new_salesperson = Salesperson.new
+
+new_salesperson["first_name"] = "Tom"
+new_salesperson["last_name"] = "Waits"
+new_salesperson["email"] = "claphands@gmail.com"
+new_salesperson.save
+
+#puts new_salesperson.inspect
+
+
 # 3. write code to display how many salespeople rows are in the database
 
 # ---------------------------------
 # Salespeople: 2
 
+puts "Salespeople: #{Salesperson.all.count}"
+
 # 4. modify/update column data for a row in the salespeople table.
+
+tom_waits = Salesperson.find_by({"last_name" => "Waits"})
+#puts twitter.inspect
+tom_waits["email"] = "tom.waits@gmail.com"
+tom_waits.save
+#puts tom_waits.inspect
 
 # CHALLENGE:
 # 5. write code to display each salesperson's full name
@@ -35,3 +60,14 @@ Salesperson.destroy_all
 # Salespeople: 2
 # Ben Block
 # Brian Eng
+all_salespeople = Salesperson.all
+
+for person in all_salespeople
+#puts person.inspect
+first_name = person["first_name"]
+last_name = person["last_name"]
+email = person ["email"]
+puts "#{first_name} #{last_name}: #{email}"
+
+end
+
